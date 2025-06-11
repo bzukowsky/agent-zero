c4c42d764faf1c423b0776d630aaef7a258d493b
Beth Zukowsky <bzukowsky@Beths-iMac.local>
2025-06-11 13:41:15 -0400

    removing RFC

--- python/helpers/searxng.py
+++ python/helpers/searxng.py
@@ -4,7 +4,7 @@ from python.helpers import runtime
 URL = "http://localhost:55510/search"
 
 async def search(query:str):
-    return await runtime.call_development_function(_search, query=query)
+    return await _search(query=query)
 
 async def _search(query:str):
     async with aiohttp.ClientSession() as session:
